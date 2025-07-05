5db4934bf5abc1b2014a3ba962c771fbd5efc262
sofia vidotto <sofiavidotto8@gmail.com>
2025-07-05 22:11:12 +0200

    fix on the filters by genres ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import GenresDropdown from "./GenresDropdown";
-import ReleaseYearDropdown from "./ReleaseYearDropdown";
-import AlphabeticalOrderDropdown from "./AlphabeticalOrderDropdown";
 
 export default function Sidebar({ filters, setFilters }) {
 
@@ -15,15 +13,9 @@ export default function Sidebar({ filters, setFilters }) {
 
     return (
         <aside className="w-full sm:w-64 bg-gray-900 text-white p-6 rounded-xl shadow-lg space-y-6">
-           
+        
             {/* Filtro per genere */}
             <GenresDropdown />
-
-            {/* Filtro per anno di uscita */}
-            <ReleaseYearDropdown onSelect={(year) => console.log("Anno:", year)} />
-
-            {/* Ordina alfabeticamente */}
-            <AlphabeticalOrderDropdown onSelect={(order) => console.log("Ordine:", order)} />
         </aside >
     );
 }
